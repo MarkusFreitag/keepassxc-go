4b457c1b2c44284943954603d40e3f02c49737fc
Markus Freitag <fmarkus@mailbox.org>
2022-07-14 09:44:55 +0200

    fix socket path test

--- pkg/keepassxc/client_test.go
+++ pkg/keepassxc/client_test.go
@@ -1,24 +1,18 @@
 package keepassxc_test
 
 import (
-	"os"
+	"regexp"
 	"testing"
 
-	"bou.ke/monkey"
 	"github.com/stretchr/testify/require"
 
 	"github.com/MarkusFreitag/keepassxc-go/pkg/keepassxc"
 )
 
 func TestSocketPath(t *testing.T) {
-	monkey.Patch(os.Getuid, func() int {
-		return 1000
-	})
-	defer monkey.Unpatch(os.Getuid)
-
-	require.Equal(
+	require.Regexp(
 		t,
-		"/run/user/1000/org.keepassxc.KeePassXC.BrowserServer",
+		regexp.MustCompile(`/run/user/\d+/org.keepassxc.KeePassXC.BrowserServer`),
 		keepassxc.SocketPath(),
 	)
 }
