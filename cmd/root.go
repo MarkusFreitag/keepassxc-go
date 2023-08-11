22f316125f55122da5932436868eed57a12d048d
Markus Freitag <fmarkus@mailbox.org>
2023-08-11 17:34:25 +0200

    cmd: dont initialize the client within a persistentprerun (#14)

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,76 +28,11 @@ import (
 	"github.com/MarkusFreitag/keepassxc-go/pkg/keystore"
 )
 
-var (
-	profileName string
-	client      *keepassxc.Client
-)
+var profileName string
 
 var rootCmd = &cobra.Command{
 	Use:   "keepassxc-go",
 	Short: "interact with keepassxc via unix-socket",
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		socketPath, err := keepassxc.SocketPath()
-		if err != nil {
-			return err
-		}
-
-		store, err := keystore.Load()
-		if err != nil {
-			return err
-		}
-
-		var key nacl.Key
-		switch len(store.Profiles) {
-		case 0:
-			break
-		case 1:
-			key = store.Profiles[0].NaclKey()
-			profileName = store.Profiles[0].Name
-		default:
-			if profileName == "" {
-				return errors.New("keystore has multiple profiles, please specify the one to use")
-			}
-			for _, profile := range store.Profiles {
-				if profile.Name == profileName {
-					key = profile.NaclKey()
-				}
-			}
-			if key == nil {
-				return fmt.Errorf("could not find profile '%s'", profileName)
-			}
-		}
-
-		client = keepassxc.NewClient(socketPath, profileName, key)
-		if err := client.Connect(); err != nil {
-			return err
-		}
-
-		if err := client.ChangePublicKeys(); err != nil {
-			return err
-		}
-
-		if key == nil {
-			if err := client.Associate(); err != nil {
-				return err
-			}
-			name, key := client.GetAssociatedProfile()
-			err = store.Add(&keystore.Profile{Name: name, Key: key})
-			if err != nil {
-				return err
-			}
-			err = store.Save()
-			if err != nil {
-				return err
-			}
-		} else {
-			if err := client.TestAssociate(); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	},
 }
 
 func Execute() {
@@ -116,4 +51,67 @@ func init() {
 	rootCmd.SilenceErrors = true
 
 	rootCmd.PersistentFlags().StringVarP(&profileName, "profile", "p", "", "Only necessary if keystore contains multiple profiles")
+}
+
+func initializeClient() (*keepassxc.Client, error) {
+	socketPath, err := keepassxc.SocketPath()
+	if err != nil {
+		return nil, err
+	}
+
+	store, err := keystore.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	var key nacl.Key
+	switch len(store.Profiles) {
+	case 0:
+		break
+	case 1:
+		key = store.Profiles[0].NaclKey()
+		profileName = store.Profiles[0].Name
+	default:
+		if profileName == "" {
+			return nil, errors.New("keystore has multiple profiles, please specify the one to use")
+		}
+		for _, profile := range store.Profiles {
+			if profile.Name == profileName {
+				key = profile.NaclKey()
+			}
+		}
+		if key == nil {
+			return nil, fmt.Errorf("could not find profile '%s'", profileName)
+		}
+	}
+
+	client := keepassxc.NewClient(socketPath, profileName, key)
+	if err := client.Connect(); err != nil {
+		return nil, err
+	}
+
+	if err := client.ChangePublicKeys(); err != nil {
+		return nil, err
+	}
+
+	if key == nil {
+		if err := client.Associate(); err != nil {
+			return nil, err
+		}
+		name, key := client.GetAssociatedProfile()
+		err = store.Add(&keystore.Profile{Name: name, Key: key})
+		if err != nil {
+			return nil, err
+		}
+		err = store.Save()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		if err := client.TestAssociate(); err != nil {
+			return nil, err
+		}
+	}
+
+	return client, nil
 }
